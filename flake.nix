53f820b7887513613d3f1d6f92a5385555f2bece
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-11-29 11:34:00 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,9 +7,26 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Add inputs for your dotfile repositories
+    nvim-config = {
+      url = "github:yourusername/nvim-config";
+      flake = false;  # Just fetch the source
+    };
+
+    tmux-config = {
+      url = "github:yourusername/tmux-config";
+      flake = false;
+    };
+
+    i3-config = {
+      url = "github:yourusername/i3-config";
+      flake = false;
+    };
+
   };
 
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = { self, nixpkgs, home-manager, nvim-config, tmux-config, i3-config }:
     let
       # This needs to be adjusted based on your system
       system = "x86_64-linux";  
@@ -17,7 +34,17 @@
     in {
       homeConfigurations.mydevenv = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./home.nix ];
+        modules = [ 
+          ./home.nix 
+          {
+            # Pass the inputs to home-manager configuration
+            home.file = {
+              ".config/nvim".source = nvim-config;
+              ".config/tmux".source = tmux-config;
+              ".config/i3".source = i3-config;
+            };
+          }
+        ];
       };
     };
 }
